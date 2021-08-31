fe6c9e1ddca5ef8db3f1a4c2ee4a5944b410261e
ShevamNagar <89196117+ShevamNagar@users.noreply.github.com>
2021-08-31 15:11:33 +0530

    Add files via upload

--- ems stored procedure.sql
+++ ems stored procedure.sql
@@ -29,5 +29,53 @@ insert into Departments (Name) values (@name)
 
 end
 
+--////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+create proc usp_DepartmentList
+as
+begin
+	select*from Departments
+end
+
+--////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+create proc usp_AddEmployee @name nvarchar(50),
+							@email nvarchar(50),
+							@salary money,
+							@dateofjoining date,
+							@gender int,
+							@hobbies nvarchar(50),
+							@depatmentId int 
+as
+begin
+	--validate
+	if(LEN(@name) = 0)
+		throw 500010, 'Name cannot be Empty',1
+	if(@salary <0)
+		throw 500011, 'Salaray cannot be < 0',1
+	if(LEN(@email) = 0)
+		throw 500012, 'Email cannot be Empty',1
+	if(LEN(@hobbies) = 0)
+		throw 500013, 'Hobbies cannot be Empty',1
+
+	if(exists(select Email from Employees where Email = @email))
+		throw 500020, 'Employee already exist',1
+
+	if(not exists(select Id from Departments where Id = @depatmentId))
+		throw 500030, 'Department ID not Valid',1
+
+insert into Employees (Name, DateofJoining, Salary, Email, Gender, Hobbies, DepartmentId)
+values (@name,@dateofjoining,@salary,@email,@gender,@hobbies,@depatmentId)
+end
+
+
+--////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+create proc usp_EmployeeList
+as
+begin
+	select*from Employees
+end
+
+--////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 select * from UserInfo
 select* from Departments
+select * from Employees
